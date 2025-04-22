2412311367cb667215a5f8f9c35fd01b33e77e32
Jersson Tovar <jerssonfelipetovarpatino@gmail.com>
2025-04-22 07:25:40 -0500

    microservicios funcionando, implementación de keycloak

---
+++ migrations/20250401045335-create-empleado.js
@@ -0,0 +1,34 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Empleados', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nombre: {
+        type: Sequelize.STRING
+      },
+      cedula: {
+        type: Sequelize.STRING
+      },
+      cargo: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Empleados');
+  }
+};
